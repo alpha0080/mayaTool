ff78e35d9213caeaeea4de867f81d9fb0259ca58
alphaelf0080 <shau_ren_chiou@hotmail.com>
2017-06-06 22:58:07 +0800

    publish flow

---
+++ publishTool/publishFlow_01.py
@@ -0,0 +1,28 @@
+
+
+sourceFileName = ''
+destinationFileName = ''
+
+
+def getSourceFileName(self):
+    print "getSourceFileName"
+    
+def destinationFileName(self):
+    print "destinationFileName"
+    
+    
+#define publish folder location
+
+#collect files, textures, file path
+
+    #define input files list
+    #rename nodes
+    #copy input files to new location, publish/assets/assetName/sourceimages  
+    #relocation input files path
+    
+    
+#save file to publish folder, publish/assets/assetName/fileName.mb both fileName.ma
+
+#fileInfo export ,description , icons
+
+    
