91197797d616d4f9496bd3349bd5a76cbd4dd12c
alphaelf0080 <shau_ren_chiou@hotmail.com>
2017-06-12 22:58:37 +0800

    dataTransfer

--- publishTool/dataTransfer_test_01.py
+++ publishTool/dataTransfer_test_01.py
@@ -3,30 +3,36 @@ import json
 from pprint import pprint
 
 
-fileName = "//mcd-server/art_3d_project/ani_pop_v01_cf/global/assets/character/chicken/chicken_model.json"
-
+fileName = 'C:/mayaProjs/ani_pop_v01_cf/global/shot/shot02/shot02_lighting.json'
 with open(fileName) as data_file:    
     data = json.load(data_file)
     
 bb= {'1': {u'g': {u'dgdg': {u'gg': {}}}}, '0': {'master': {}}}   
 
+print data.keys()
+
+#dd= {u'0': {u'master': {}}, u'1': {u'g': {}}} 
 
 
-dd= {u'0': {u'master': {}}, u'1': {u'g': {}}} 
-print dd['1'].keys()
-print bb['1'].keys()
-
-
-
-cc= {}
-print cc.keys()
-for top in data.keys():
-    print 'top',top,type(top)
-    print data[top]
-    cc.update({top:{}})
-    print 'data[top].keys()',data[top].keys()
-    for sec in data[top].keys():
-        cc.update({top:{data[top][sec}})
+#indexNum = sorted(data.keys())
+#print indexNum
+branchInfoDict = {'0':{'master':{}}}
+#branchInfoDict = {}
+for i in range(1,len(data.keys())):
+    top = data[str(i)].keys()[0]
+   # print top
+    branchInfoDict.update({str(i):{top:{}}})
+    #print i,top,data[str(i)][top]['folder'].keys()
+    for sec in data[str(i)][top]['folder'].keys():
+      #  print sec
+       # branchInfoDict[str(i)][top]
+        branchInfoDict[str(i)][top].update({sec:{}})
         
+        for third in data[str(i)][top]['folder'][sec]['folder'].keys():
+           #     print i,top,sec, data[str(i)][top]['folder'][sec]['folder'].keys()
+          #  print data[str(i)][top]['folder'][sec]['folder']
+         #   print str(i),top,sec,third
+            branchInfoDict[str(i)][top][sec].update({third:{}})
+
 
 
