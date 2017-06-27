af4de9b26bec964eb545f9abdf93cd0ef29aa1d0
alphaelf0080 <shau_ren_chiou@hotmail.com>
2017-06-27 23:01:31 +0800

    class publishDescriptionClass_01

--- publishTool/publishDescriptionClass_01.py
+++ publishTool/publishDescriptionClass_01.py
@@ -1,4 +1,49 @@
 
+
+class publishFileState:
+    
+    def __init__(self,keyFileName):
+        
+        self.keyFileName = keyFileName
+        
+    def fileState(self):
+        #typical fileName = projInt_assetTypeInit_assetName_assetProcessInit_master.mb
+        #example: ow_cf_c_angelFish_mod_master.mb
+        
+        typicalDict ={{'projectWorkSpace':'workSpace'},
+                      {'fileName':'fullFilePath'},
+                      {'fileIcon':'fullIconPath'},
+                      {'ribArchive':{'ribArchivePath':{'high':{},
+                                                       'mid':{},
+                                                       'low':{}},
+                                     'parentGpu':{'high':{},
+                                                  'mid':{},
+                                                  'low':{}},
+                                     'startFrame':{},
+                                     'endFrame':{},
+                                     'frameRate':{},
+                                     'isMotionBlur':{}}
+                      {'gpuCache':'gpuCachePath'},
+                      {'publishTime':'time'},
+                      {'metaData':{}},
+                      {'user':'userName'}
+                      }
+                      
+        
+        
+    def printOut(self):
+        
+        print 'fileName:', self.keyFileName
+        
+
+
+aa= publishFileState('asd')
+
+aa.printOut()
+
+    
+    
+
 def run(self):
     self.buildPublishDescriptionDictStructure()
     self.output()
@@ -28,7 +73,25 @@ def buildPublishDescriptionDictStructure(self):
                                                         'shot04':[]}
                                                 }
                                     }
-                                                        
+
+
+def publishFileStateKey(self):
+    fileStateKey=[{'fileName':'fileParh'},
+                  {'fileIcon':'iconPath'},
+                  {'ribArchive':'ribPath'},
+                  {'fileData':'publlishTime'},
+                  {'fileDescription':''}]       
+    
+    
+    
+    
+                                       
+
+aa= json.dumps(assetName, sort_keys=True , indent =4)  
+pprint(aa) 
+import json ,pprint
+
+
                                                                    
 def output(self):
     print self.projPubishDescriptionDict
