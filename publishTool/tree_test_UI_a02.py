19ee50de4d7d5573d1c24c03673eec2dbde38826
alphaelf0080 <shau_ren_chiou@hotmail.com>
2017-08-28 00:56:46 +0800

    treeTest

--- publishTool/tree_test_UI_a02.py
+++ publishTool/tree_test_UI_a02.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:/Users/alpha/Documents/GitHub/test/QTtest/fileDir_test_02/tree_test_UI_a01.ui'
+# Form implementation generated from reading ui file 'C:/Users/alpha/Documents/GitHub/mayaTool/publishTool/tree_test_UI_a02.ui'
 #
-# Created: Thu Aug 10 22:41:45 2017
+# Created: Mon Aug 14 22:42:38 2017
 #      by: pyside2-uic  running on PySide2 2.0.0~alpha0
 #
 # WARNING! All changes made in this file will be lost!
@@ -27,6 +27,12 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(410, 40, 80, 111))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(410, 180, 80, 111))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(410, 350, 80, 111))
+        self.pushButton_3.setObjectName("pushButton_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -47,7 +53,9 @@ class Ui_MainWindow(object):
         self.treeWidget.topLevelItem(1).setText(0, QtWidgets.QApplication.translate("MainWindow", "b", None, -1))
         self.treeWidget.topLevelItem(2).setText(0, QtWidgets.QApplication.translate("MainWindow", "c", None, -1))
         self.treeWidget.setSortingEnabled(__sortingEnabled)
-        self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "PushButton", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "A", None, -1))
+        self.pushButton_2.setText(QtWidgets.QApplication.translate("MainWindow", "B", None, -1))
+        self.pushButton_3.setText(QtWidgets.QApplication.translate("MainWindow", "C", None, -1))
 
 
 
@@ -59,62 +67,6 @@ class mod_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         #self.QTITEM.ACTION.connect(self.MODDEF)
         self.setupUi(self)
     #def self.MODDEF(self):
-    
-        self.pushButton.clicked.connect(self.runTest)
-        
-    def runTest(self):
-        
-        self.getAllGrpTransformFromOutliner('character')
-        
- 
-    def getAllGrpTransformFromOutliner(self,topLayerItemInOutlinear):
-
-        tempBuildDefaultGrpList = []
-        countList= []
-            
-        allInTransformGrp = cmds.listRelatives(topLayerItemInOutlinear, children=True, pa=True,ad=True)
-            #print allInTransformGrp
-        if allInTransformGrp == None:
-            pass
-        else:
-            for j in allInTransformGrp:
-                grpLingName =  cmds.ls(j,l=True)[0]
-                tempBuildDefaultGrpList.append(grpLingName)
-              #  print grpLingName
-                countList.append(len(grpLingName.split('|')))
-                
-        maxDepth = sorted(countList)[-1]
-
-        elementEveryLevelCount = {}
-        itemHierarchy = {}
-        for i in range(1,maxDepth):
-            elementEveryLevelCount.update({i:[]})
-        for i in tempBuildDefaultGrpList:
-            chaList =  i.split('|')
-           # print 'chaList',chaList
-            #getItemHierarchy(chaList,maxDepth,elementEveryLevelCount)
-          #  getItemHierarchyB(chaList,itemHierarchy)
-        cha = tempBuildDefaultGrpList[0]
-      #  print 'cha',cha
-        getItemHierarchyC(cha,itemHierarchy)
-        
-    def getItemHierarchyC(cha, itemHierarchy):
-        print cha   
-
-        
-      #  print 'elementEveryLevelCount',elementEveryLevelCount
-       # print 'itemHierarchy',itemHierarchy
-        
-        
-    def createTree(self):
-        print 'aaaaa'
-        
-        self.treeWidget.clear()
-        
-      #  item_0 = QtWidgets.QTreeWidgetItem(self.treeWidget)
-        item = QtWidgets.QTreeWidgetItem(self.treeWidget)
-        item.setText(0,QtWidgets.QApplication.translate("MainWindow", "ccc", None, -1))
-        item2 =QtWidgets.QTreeWidgetItem(item)
 
 
 
